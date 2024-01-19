6846c04ba98f8cb181c087ff2e262d5260548472
zehreken <zehreken@gmail.com>
2024-01-19 23:32:29 +0100

    Add basic folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use chrono::{self, DateTime, Datelike, Local, Timelike, Utc};
-use std::{fs::File, io::Write, os::unix::process::CommandExt, process::Command};
+use std::{
+    fs::{create_dir, File},
+    io::Write,
+    os::unix::process::CommandExt,
+    path::Path,
+    process::Command,
+};
 
 fn main() {
     let now = Local::now();
@@ -13,13 +19,28 @@ fn main() {
     );
     println!("Work on journaling app is in progress");
 
+    let year_folder_name = format!("{:04}", now.year());
+    let year_folder_path = Path::new(&year_folder_name);
+    if !Path::exists(&year_folder_path) {
+        create_dir(&year_folder_path).expect("Error while creating year folder");
+    }
+    let month_folder_name = format!("{:02}", now.month());
+    let month_folder_path = year_folder_path.join(&month_folder_name);
+    if !Path::exists(&month_folder_path) {
+        create_dir(&month_folder_path).expect("Error while creating month folder");
+    }
     let date = format!("{:04}-{:02}-{:02}", now.year(), now.month(), now.day());
     let file_name = format!("{}.toml", date);
-    let mut file = File::create(&file_name).expect("Error while creating file");
-    let init = format!("date = \"{}\"\nmarkdown = \"\"\"\n\"\"\"", date);
-    file.write_all(init.as_bytes())
-        .expect("Error while writing to file");
+    let file_path = month_folder_path.join(&file_name);
+    if !Path::exists(&file_path) {
+        let mut file = File::create(&file_path).expect("Error while creating file");
+        let init = format!("date = \"{}\"\nmarkdown = \"\"\"\n\"\"\"", date);
+        file.write_all(init.as_bytes())
+            .expect("Error while writing to file");
+    } else {
+        println!("Today's journal already exists")
+    }
 
     let mut output = Command::new("vim");
-    output.arg(&file_name).status().expect("Error starting Vim");
+    output.arg(&file_path).status().expect("Error starting Vim");
 }
